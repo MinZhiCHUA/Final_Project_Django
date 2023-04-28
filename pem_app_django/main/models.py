6bf33d3618b2906c4462036ae8ee7442c364ae4a
MinZhi_Chua <min-zhi.chua@adeo.com>
2023-04-28 09:32:28 +0200

    read write in BDD, delete, etc

--- pem_app_django/main/models.py
+++ pem_app_django/main/models.py
@@ -1,4 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 
 class User(AbstractUser):
     pass
+
+class Product(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    # pred_label_text = models.CharField(max_length=200)
+    # pred_label_code = models.CharField(max_length=200)
+    pred_date = models.DateTimeField("date publish", default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Feedback(models.Model):
+    feedback_txt = models.CharField(max_length=1000)
+    feedback_date = models.DateTimeField("date publish")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- pem_app_django/main/views.py
+++ pem_app_django/main/views.py
@@ -1,12 +1,110 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from .functions import multiplicate_by_5
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from .forms import ProductForm
+from .models import Product
+
 
 
 # Create your views here.
 @login_required
 def home_page(request):
     return render(request, 'main/home_page.html')
+
+# @login_required
+# def user_history(request):
+
+
+#     context = {
+#         "product_database" : Product.objects.filter(user_id=request.user),
+#     }
+
+
+#     return render(request, 'main/user_history.html', context=context)
+
+class User_History_page(TemplateView):
+    def get(self, request, *args, **kwargs):
+        context = {
+            "product_database" : Product.objects.filter(user_id=request.user),
+        }
+        # print(list(request.GET.keys())[0])
+        print('get method')
+        print(request.POST)
+        # if request.GET.keys() == 
+        return render(request, 'main/user_history.html', context=context)
+    
+    def post(self, request, *args, **kwargs):
+
+        # Delete function
+        print('Delete function')
+        # print(product_id)
+
+        context = {
+            "product_database" : Product.objects.filter(user_id=request.user),
+        }
+        # print(request.POST)
+        return render(request, 'main/user_history.html', context=context)
+
+    # def delete(self, request, *args, **kwargs):
+    #     context = {
+    #         "product_database" : Product.objects.filter(user_id=request.user),
+    #     }
+    #     print('delete method')
+    #     return render(request, 'main/user_history.html', context=context)
+
+# @login_required
+# def predict_page(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         else:
+#             form = ProductForm()
+#     return render(request, 'main/prediction.html', {'form': form})
+class Predict_page(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'main/prediction.html')
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        print(user)
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.user = user
+            product.save()
+            # form.user = get_user_model()
+            # form.save(user=get_user_model())
+        else:
+            print('form is not correct')
+            print(form.errors)
+            form = ProductForm()
+        return render(request, 'main/prediction.html', {'form': form})
+
+@login_required
+def admin_user_database_page(request):
+    user_database = get_user_model()
+    context = {
+        "user_database" : user_database.objects.all(),
+    }
+    
+    return render(request, 'main/user_database.html', context=context)
+
+@login_required
+def testing_page(request, product_id):
+    print(product_id)
+    # Delete this product_id
+    Product.objects.filter(id=product_id).delete()
+
+    context = {
+            "product_database" : Product.objects.filter(user_id=request.user),
+        }
+        # print(request.POST)
+    return render(request, 'main/user_history.html', context=context)
+
+    # return render(request, 'main/user_database.html', context=context, id=id)
 
 def multi_func(request):
     weekdays = [
